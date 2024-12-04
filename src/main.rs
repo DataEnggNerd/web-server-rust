235df9ed407d0a94dc227d574225f9606f025b14
Santhosh Solomon <solomon.santhosh@gmail.com>
2024-12-04 14:23:10 +0530

    minor refactor
    - moved html pages to pages/
    - removed dead codes

--- src/main.rs
+++ src/main.rs
@@ -17,38 +17,15 @@ fn main(){
 
 fn handle_request(mut stream: TcpStream){
     let buff_reader = BufReader::new(&stream);
-    // let http_request: Vec<_> = buff_reader
-    // .lines()
-    // .map(|result| result.unwrap())
-    // .take_while(|line| !line.is_empty())
-    // .collect();
+    
     let request_line = buff_reader.lines().next().unwrap().unwrap();
 
-    // let mut response: String = String::new();
-
-    // if request_line == "GET / HTTP/1.1"{
-    //     // println!("Request :{http_request:#?}");
-    //     let status_line = "HTTP 200 OK\r\n\r\n";
-    //     let contents = fs::read_to_string("hello.html").unwrap();
-    //     let length = contents.len();
-
-    //     response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-    // } else {
-    //     let status_line: &str = "HTTP 404 NOT FOUND\r\n\r";
-    //     let contents = fs::read_to_string("404.html").unwrap();
-    //     let length = contents.len();
-    //     response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-    // }
-
-    
-    // let status_line = if request_line == "GET / HTTP/1.1" {"HTTP 200 OK\r\n\r\n"} else {"HTTP 404 NOT FOUND\r\n\r\n"};
-    // let file_name = if request_line == "GET / HTTP/1.1" {"hello.html"} else {"404.html"};
-
     let (status_line, file_name) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP 200 OK\r\n\r\n", "hello.html")
+        ("HTTP 200 OK\r\n\r\n", "pages/200.html")
     } else {
-        ("HTTP 404 NOT FOUND\r\n\r\n", "404.html")
+        ("HTTP 404 NOT FOUND\r\n\r\n", "pages/404.html")
     };
+
     let contents = fs::read_to_string(file_name).unwrap();
     let length = contents.len(); 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
