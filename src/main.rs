4cba54d941e196eba7402a658dadf3e64d11aa1c
Santhosh Solomon <solomon.santhosh@gmail.com>
2024-12-04 19:03:40 +0530

    conditional statement refactor
    - changed from if..else..if to match pattern

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,24 @@ fn handle_request(mut stream: TcpStream){
 
     // to simulate scenario of accepting multi requests at same time
     // every other request will stall at browser
-    sleep(Duration::from_secs(15));
+    // sleep(Duration::from_secs(15));
 
-    let (status_line, file_name) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP 200 OK\r\n\r\n", "pages/200.html")
-    } else {
-        ("HTTP 404 NOT FOUND\r\n\r\n", "pages/404.html")
+    // let (status_line, file_name) = if request_line == "GET / HTTP/1.1" {
+    //     ("HTTP 200 OK\r\n\r\n", "pages/200.html")
+    // } else {
+    //     ("HTTP 404 NOT FOUND\r\n\r\n", "pages/404.html")
+    // };
+
+    // above if else block refactored to `match` statement as there are more than two scenarios
+    // no if..elif..else block in rust or not recommended
+
+    let (status_line, file_name) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP 200 OK/r/n/r/n", "pages/200.html"),
+        "GET /sleep HTTP/1.1" => {
+            sleep(Duration::from_secs(5));
+            ("HTTP 200 OK/r/n/r/n", "pages/200.html")
+        },
+        _ => ("HTTP 404 NOT FOUND/r/n/r/n", "pages/404.html")
     };
 
     let contents = fs::read_to_string(file_name).unwrap();
